7fdaecd638ce2f3b5a1850947e3b5cf89388ed79
Sergey Piterskikh <piterskikhsa@mail.ru>
2017-03-31 00:42:44 +0300

    модификация файла need_hashes1.csv
    Signed-off-by: Sergey Piterskikh <piterskikhsa@mail.ru>

--- lesson_1/homework/hashes.py
+++ lesson_1/homework/hashes.py
@@ -21,13 +21,19 @@ def create_hash(data, hash):
         print('Неверный хеш метод')
 
 
-with open('need_hashes.csv', 'r') as file:
+file_write = []
+with open('need_hashes.csv', 'r', encoding='cp1251') as file:
     reader = csv.reader(file, delimiter=';')
     for data in reader:
-        print(', '.join(data))
         result = create_hash(data[0], data[1])
         data[2] = result
-        print(data)
+        file_write.append(data)
+
+with open('need_hashes1.csv', 'a', newline='') as f:
+    writer = csv.writer(f,delimiter=';')
+    for row in file_write:
+        writer.writerow(row)
+
 
 
 class TestHash(unittest.TestCase):
@@ -35,8 +41,12 @@ class TestHash(unittest.TestCase):
         self.assertEqual(create_hash('I love Python', 'sha1'), '9233eac58259dd3a13d6c9c59f8001823b6b1fee')
 
     def test_hash_sha512(self):
-        self.assertEqual(create_hash('I love Python', 'sha512'), '9233eac58259dd3a13d6c9c59f8001823b6b1fee')
+        self.assertEqual(create_hash('Guido like beer', 'sha512'),
+                         '9490ca5a146f482ea40649b832017dbec7279462766ec6e02fac4c7910584c50572b3efd3d257c089431b8b88603226a9d3aa2fcd4185fcc8db9f71114e06d93')
 
     def test_hash_md5(self):
-        self.assertEqual(create_hash('I love Python', 'md5'), '9233eac58259dd3a13d6c9c59f8001823b6b1fee')
+        self.assertEqual(create_hash('Я люблю Питон', 'md5'), '1190b0a5e7b09ec3145999044c728854')
 
+
+if __name__ == '__main__':
+    unittest.main()
